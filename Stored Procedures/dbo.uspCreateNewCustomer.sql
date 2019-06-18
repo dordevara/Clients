8276ae8e4d0ecbcc7627570130edc1ddf110f31c
Roxana <51920808+dordevara@users.noreply.github.com>
2019-06-18 13:39:45 +0100

    init
    few small changes

--- Stored Procedures/dbo.uspCreateNewCustomer.sql
+++ Stored Procedures/dbo.uspCreateNewCustomer.sql
@@ -3,12 +3,12 @@ GO
 SET ANSI_NULLS ON
 GO
 -- =============================================
--- Author:		Roxana Butenco
+-- Author: Roxana Butenco
 -- Create date: 14/06/2019
 -- Description:	Create a new customer and a new Association with an existing customer
 -- =============================================
 CREATE PROCEDURE [dbo].[uspCreateNewCustomer]
-	-- Add the parameters for the stored procedure here
+	-- Add the parameters for the stored procedure 
 (  
 	@FName VARCHAR(20),
 	@LName VARCHAR(20),
@@ -29,9 +29,10 @@ BEGIN
 
     -- Insert statements for procedure here
 	INSERT INTO Clients	(ClientRefId, FName, LName, DateOfBirth, CompanyName, City, Country, PhoneNo) 
-		VALUES			(@ClientRefId, @FName, @LName, @DateOfBirth, @CompanyName, @City, @Country, @PhoneNo)  
-	DECLARE		@ClientId INT = @@IDENTITY  
-	INSERT INTO Associations (Client1, AssocRefId, Client2) 
+		VALUES		(@ClientRefId, @FName, @LName, @DateOfBirth, @CompanyName, @City, @Country, @PhoneNo)  
+	
+	DECLARE	@ClientId INT = @@IDENTITY  
+	INSERT INTO Associations 	(Client1, AssocRefId, Client2) 
 		VALUES			(@ClientId, @AssocRefId, @Client2) 
 END
 GO
